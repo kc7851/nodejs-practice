d2046c1197a3414760351d2c1089808331647f86
KiChang Shin <kc7851@gmail.com>
2019-04-23 00:01:52 +0900

    nodejs ing... pm2 install pm2 start main.js --watch pm2 log pm2 monit pm2 stop main pm2 kill etc...

---
+++ function.js
@@ -0,0 +1,7 @@
+function sum(first, second){
+    console.log(first + second);
+}
+
+sum(1); //NaN
+sum(1,3); //4
+sum(1,3,5); //4
--- main.js
+++ main.js
@@ -2,74 +2,71 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+  return `
+      <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `
+}
+function templateList(fileList) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < fileList.length) {
+    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+    i++;
+  }
+  list = list + '</ul>';
+  return list;
+}
 var app = http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathName = url.parse(_url, true).pathname;
-    
-    // console.log(pathName);
-    
-    if(pathName === '/'){
-        if(queryData.id === undefined){
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var template = `
-            <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-                response.writeHead(200);
-                response.end(template);
-            });
-        } else{
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                var title = queryData.id;
-                var template = `
-            <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-                response.writeHead(200);
-                response.end(template);
-            });
-        }
-        
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathName = url.parse(_url, true).pathname;
+
+  // console.log(pathName);
+
+  if (pathName === '/') {
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (error, fileList) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = templateList(fileList);
+
+        var template = templateHTML(title, list, `<h2>${title}</h2>
+        <p>${description}</p>`);
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
-        response.writeHead(404);
-        response.end('Not found');
+      fs.readdir('./data', function (error, fileList) {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          var title = queryData.id;
+          var list = templateList(fileList);
+          var template = templateHTML(title, list, `<h2>${title}</h2>
+          <p>${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
     }
 
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 
-    
+
+
 
 
 });
